2cf2663b01e9cce283b858608a5a5d7cc434587a
Yash malviya <yashno25@gmail.com>
2024-12-15 00:38:08 +0530

    updating code

--- tictactoe/src/Strategies/RowRule.java
+++ tictactoe/src/Strategies/RowRule.java
@@ -1,11 +1,25 @@
 package Strategies;
 
 import Models.Board;
+import Models.Cell;
+import Models.CellState;
 import Models.Move;
+
+import java.util.List;
 
 public class RowRule implements  Rules{
     @Override
     public boolean checkWinner(Board board, Move move) {
-        return false;
+
+        int row = move.getCell().getRow();
+        List<List<Cell>> grid = board.getGrid();
+        boolean result = true;
+        for(int i = 0;i<3;i++){
+            Cell currentCell = grid.get(row).get(i);
+            if(currentCell.getState() == CellState.Empty){
+                result = false;
+            }
+        }
+        return result;
     }
 }
